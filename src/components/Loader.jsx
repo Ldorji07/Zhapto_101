1e9b9d55e44c27ca0fd054c8a5d5ce169fc8c18c
ldorji07 <ldorji705@gmail.com>
2025-10-23 15:56:25 +0600

    updated

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => (
+  <div className="loader-container">
+    <div className="loader-circle"></div>
+    <h2 className="loader-text">Loading...</h2>
+  </div>
+);
+
+export default Loader;
